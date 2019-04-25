318e24c0220c36767be9bea5f2751397e728b680
Noah Roberson <ncroberson@csu.fullerton.edu>
2019-04-24 21:24:21 -0700

    updated upload.js again

--- upload.js
+++ upload.js
@@ -6,21 +6,18 @@
   var myFile;
 
   $('#fileupload').on('submit', function () {
-    myFile = $('#fileinput'.prop('files');  
+    myFile = $('#fileinput'.prop('files'));
   });
 
-  var Minio = require('minio')
+  var Minio = require('minio');
 
   var minioClient = new Minio.Client({
-    endPoint: 'localhost',
+    endPoint: 'localhost/minio/images',
     port: 9000,
     useSSL: true,
     accessKey: 'ATI3FBPPL3KMK9S7GNXX',
     secretKey: 'RvwNI+cmnQQc6nyo1QWG84kXZJVNZALbbiRMAHhq'
-});
+  });
 
   minioClient.putObject(myFile);
-
-
-
 })(window);
