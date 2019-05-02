14dee36802e5a1312912bb8c72c1b00841c0232d
Noah Roberson <ncroberson@csu.fullerton.edu>
2019-05-01 19:37:07 -0700

    updated code for standard JS compatability

--- images.js
+++ images.js
@@ -1,19 +1,16 @@
 (function (window) {
-    'use strict';
-    
-    var pict = '[data-posts="Pic"]';
-    var capt = '[data-posts="Cap"]';
-
-    $.ajax({
-      type: 'GET',
-      url: 'http://localhost:3000/images',
-      success: function (data) {
-        console.log("success", data)
-        $.each(data, function (i, item) { 
-          console.log(item.src);
-          $('#Hello').append('<img src="' + item.src + '">');
-          $('#cap').append(item.caption);
-        });
-      }
+  'use strict';
+  const $ = window.$;
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:3000/images',
+    success: function (data) {
+      console.log('success', data);
+      $.each(data, function (i, item) {
+        console.log(item.src);
+        $('#Hello').append('<img src="' + item.src + '">');
+        $('#cap').append(item.caption);
+      });
+    }
   });
-  })(window);
+})(window);
