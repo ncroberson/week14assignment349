9f5ca78321bbd8ad3feabd18851f406a3487413c
Noah Roberson <ncroberson@csu.fullerton.edu>
2019-05-01 21:37:11 -0700

    Update upload.js

--- upload.js
+++ upload.js
@@ -3,21 +3,34 @@
 
   const $ = window.$;
 
-  var myFile;
-
-  $('#fileupload').on('submit', function () {
-    myFile = $('#fileinput'.prop('files'));
+  
+  $(document).ready ( function () {
+    $('#submitbutton').click(function () {
+      let myFile;
+      myFile = $('#fileinput').prop('files')[0];
+      let AWS = window.AWS;
+      let s3Client = new AWS.S3({
+        endpoint: 'http://127.0.0.1:9000',
+        s3ForcePathStyle: true,
+        signatureVersion: 'v4',
+        accessKeyID: 'ATI3FBPPL3KMK9S7GNXX',
+        secretAccessKey: 'RvwNI+cmnQQc6nyo1QWG84kXZJVNZALbbiRMAHhq',
+        sslEnabled: false,
+        region: 'us-east-1'
+      });
+    
+      s3Client.putObject({
+        Bucket: "images",
+        Key: myFile.name,
+        Body: myFile,
+        ContentLength: myFile.size,
+        ContentType: myFile.type
+      }, function(err, data) {
+        console.log(err, data);
+      });
+    });
   });
+    
 
-  var Minio = window.Minio;
 
-  var minioClient = new Minio.Client({
-    endPoint: 'localhost/minio/images',
-    port: 9000,
-    useSSL: true,
-    accessKey: 'ATI3FBPPL3KMK9S7GNXX',
-    secretKey: 'RvwNI+cmnQQc6nyo1QWG84kXZJVNZALbbiRMAHhq'
-  });
-
-  minioClient.putObject(myFile);
 })(window);
