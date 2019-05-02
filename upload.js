6dcaa7676c5efec5af1e6dabac3341716cc5515d
abheysingh96 <abheysingh96@csu.fullerton.edu>
2019-05-01 21:40:59 -0700

    Add files via upload

--- upload.js
+++ upload.js
@@ -1,23 +1,37 @@
-(function (window) {
-  'use strict';
+let s3Client = new AWS.S3({
+  endpoint: "http://127.001:9000",
+  s3ForcePathStyle: true,
+  signatureVersion: "v4",
+  accessKeyID: "ATI3FBPPL3KMK9S7GNXX ",
+  secretaccessKey: "RvwNI+cmnQQc6nyo1QWG84kXZJVNZALbbiRMAHhq"
+});
 
-  const $ = window.$;
+s3Client.putObject({
+  Bucket: "images",
+  Key: file.name,
+  Body: file,
+  ContentLength: file.size,
+  ContentType: file.type
+}, function(err, data){
+  console.log(err, data)
+});
 
-  var myFile;
+class UpLoadForm extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = "<h1>Upload Your Photos</h1> <br> <h2>Name:</h2>";
+    var button = document.querySelector("button");
 
-  $('#fileupload').on('submit', function () {
-    myFile = $('#fileinput'.prop('files'));
-  });
+    button.addEventListener('click', function (event) {
+      button.textContent ='Uploaded';
 
-  var Minio = window.Minio;
+      var caption = document.getElementById("text").value;
+      var file = document.getElementById("file").value;
 
-  var minioClient = new Minio.Client({
-    endPoint: 'localhost/minio/images',
-    port: 9000,
-    useSSL: true,
-    accessKey: 'ATI3FBPPL3KMK9S7GNXX',
-    secretKey: 'RvwNI+cmnQQc6nyo1QWG84kXZJVNZALbbiRMAHhq'
-  });
+      uploadPicture(file, caption);
+    });
+  }
+}
 
-  minioClient.putObject(myFile);
-})(window);
+customElements.define('upload-form', UpLoadForm);
+
+
