ef916257175e0ae39f6e9f6feb516406bf8ead82
Noah Roberson <ncroberson@gmail.com>
2019-05-01 21:46:32 -0700

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,37 +1,28 @@
-let s3Client = new AWS.S3({
-  endpoint: "http://127.001:9000",
-  s3ForcePathStyle: true,
-  signatureVersion: "v4",
-  accessKeyID: "ATI3FBPPL3KMK9S7GNXX ",
-  secretaccessKey: "RvwNI+cmnQQc6nyo1QWG84kXZJVNZALbbiRMAHhq"
-});
-
-s3Client.putObject({
-  Bucket: "images",
-  Key: file.name,
-  Body: file,
-  ContentLength: file.size,
-  ContentType: file.type
-}, function(err, data){
-  console.log(err, data)
-});
-
-class UpLoadForm extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = "<h1>Upload Your Photos</h1> <br> <h2>Name:</h2>";
-    var button = document.querySelector("button");
-
-    button.addEventListener('click', function (event) {
-      button.textContent ='Uploaded';
-
-      var caption = document.getElementById("text").value;
-      var file = document.getElementById("file").value;
-
-      uploadPicture(file, caption);
+  $(document).ready ( function () {
+    $('#submitbutton').click(function () {
+      let myFile;
+      myFile = $('#fileinput').prop('files')[0];
+      let AWS = window.AWS;
+      let s3Client = new AWS.S3({
+        endpoint: 'http://127.0.0.1:9000/data/',
+        s3ForcePathStyle: true,
+        signatureVersion: 'v4',
+        accessKeyID: 'ATI3FBPPL3KMK9S7GNXX',
+        secretAccessKey: 'RvwNI+cmnQQc6nyo1QWG84kXZJVNZALbbiRMAHhq',
+        sslEnabled: false,
+        region: 'us-east-1'
+      });
+    
+      s3Client.putObject({
+        Bucket: "images",
+        Key: myFile.name,
+        Body: myFile,
+        ContentLength: myFile.size,
+        ContentType: myFile.type
+      }, function(err, data) {
+        console.log(err, data);
+      });
     });
-  }
-}
-
-customElements.define('upload-form', UpLoadForm);
+  });
 
 
