a2dce8dcb5d26874865169c6f4737a3dc35147e6
Eric Guzman <striderkills@gmail.com>
2019-05-01 21:43:13 -0700

    added templates to images

--- images.js
+++ images.js
@@ -1,6 +1,7 @@
 (function (window) {
   'use strict';
   const $ = window.$;
+
   $.ajax({
     type: 'GET',
     url: 'http://localhost:3000/images',
@@ -8,9 +9,18 @@
       console.log('success', data);
       $.each(data, function (i, item) {
         console.log(item.src);
+
         $('#Hello').append('<img src="' + item.src + '">');
         $('#cap').append(item.caption);
+
+        $('#Photo').append('<img src="' + item.src + '">');
+        $('#Capt').append(item.caption);
       });
     }
   });
+
+  const template = document.querySelector('template');
+  const node = document.importNode(template.content, true);
+  document.body.appendChild(node);
+
 })(window);
